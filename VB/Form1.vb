e65fd06f50a7a06087171a2f8a28900d1785f7cf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Form1.vb
@@ -0,0 +1,119 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraRichEdit.API.Native
+
+Namespace RichEditSwitchListFormat
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+
+            richEditControl1.Text = StringSample.SampleText
+            DefineAbstractLists(richEditControl1.Document)
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+            ApplyNextListToSelection(richEditControl1.Document)
+        End Sub
+
+        Private Sub DefineAbstractLists(ByVal document As Document)
+            document.BeginUpdate()
+
+            ' Bulleted list
+            Dim abstractListBulleted As AbstractNumberingList = document.AbstractNumberingLists.Add()
+            abstractListBulleted.NumberingType = NumberingType.Bullet
+
+            Dim level As ListLevel = abstractListBulleted.Levels(0)
+            level.ParagraphProperties.LeftIndent = 150
+            level.ParagraphProperties.FirstLineIndentType = ParagraphFirstLineIndent.Hanging
+            level.ParagraphProperties.FirstLineIndent = 75
+            level.CharacterProperties.FontName = "Symbol"
+            level.DisplayFormatString = New String(ChrW(&H00B7), 1)
+
+            ' Numbered list with decimal format
+            Dim abstractListNumberingDecimal As AbstractNumberingList = document.AbstractNumberingLists.Add()
+            abstractListNumberingDecimal.NumberingType = NumberingType.Simple
+
+            level = abstractListNumberingDecimal.Levels(0)
+            level.ParagraphProperties.LeftIndent = 150
+            level.ParagraphProperties.FirstLineIndentType = ParagraphFirstLineIndent.Hanging
+            level.ParagraphProperties.FirstLineIndent = 75
+            level.Start = 1
+            level.NumberingFormat = NumberingFormat.Decimal
+            level.DisplayFormatString = "{0}."
+
+            ' Numbered list with letter format
+            Dim abstractListNumberingLetter As AbstractNumberingList = document.AbstractNumberingLists.Add()
+            abstractListNumberingLetter.NumberingType = NumberingType.Simple
+
+            level = abstractListNumberingLetter.Levels(0)
+            level.ParagraphProperties.LeftIndent = 150
+            level.ParagraphProperties.FirstLineIndentType = ParagraphFirstLineIndent.Hanging
+            level.ParagraphProperties.FirstLineIndent = 75
+            level.Start = 1
+            level.NumberingFormat = NumberingFormat.UpperLetter
+            level.DisplayFormatString = "{0}."
+
+            ' Numbered list with roman format
+            Dim abstractListNumberingRoman As AbstractNumberingList = document.AbstractNumberingLists.Add()
+            abstractListNumberingRoman.NumberingType = NumberingType.Simple
+
+            level = abstractListNumberingRoman.Levels(0)
+            level.ParagraphProperties.LeftIndent = 150
+            level.ParagraphProperties.FirstLineIndentType = ParagraphFirstLineIndent.Hanging
+            level.ParagraphProperties.FirstLineIndent = 75
+            level.Start = 1
+            level.NumberingFormat = NumberingFormat.UpperRoman
+            level.DisplayFormatString = "{0}."
+
+            document.EndUpdate()
+        End Sub
+
+        Private Sub ApplyNextListToSelection(ByVal document As Document)
+            document.BeginUpdate()
+
+            Dim paragraphs As ReadOnlyParagraphCollection = document.Paragraphs.Get(document.Selection)
+
+            For i As Integer = 0 To paragraphs.Count - 1
+                Dim currentAbstractIndex As Integer = GetParagraphAbstractIndex(document, paragraphs(i))
+                Dim previousParagraph As Paragraph = Nothing
+
+                If i > 0 Then
+                    previousParagraph = paragraphs(i - 1)
+                End If
+
+                paragraphs(i).ListIndex = GetNextListIndex(document, currentAbstractIndex, previousParagraph)
+            Next i
+
+            document.EndUpdate()
+        End Sub
+
+        Private Function GetNextListIndex(ByVal document As Document, ByVal currentAbstractIndex As Integer, ByVal previousParagraph As Paragraph) As Integer
+            currentAbstractIndex += 1
+
+            If currentAbstractIndex = document.AbstractNumberingLists.Count Then
+                Return -1
+            Else
+                If previousParagraph IsNot Nothing AndAlso currentAbstractIndex = GetParagraphAbstractIndex(document, previousParagraph) Then
+                    Return previousParagraph.ListIndex
+                End If
+
+                Dim newList As NumberingList = document.NumberingLists.Add(currentAbstractIndex)
+
+                newList.Levels(0).SetOverrideStart(True)
+                newList.Levels(0).NewStart = 1
+
+                Return newList.Index
+            End If
+        End Function
+
+        Private Function GetParagraphAbstractIndex(ByVal document As Document, ByVal paragraph As Paragraph) As Integer
+            If Not paragraph.IsInList Then
+                Return -1
+            End If
+
+            Return document.NumberingLists(paragraph.ListIndex).AbstractNumberingListIndex
+        End Function
+    End Class
+End Namespace
---
+++ VB/StringSample.vb
@@ -0,0 +1,23 @@
+﻿Namespace RichEditSwitchListFormat
+    Public NotInheritable Class StringSample
+
+        Private Sub New()
+        End Sub
+
+        Public Shared ReadOnly Property SampleText() As String
+            Get
+                Return "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, " & ControlChars.CrLf & _
+"sed diam nonummy nibh euismod tincidunt ut laoreet dolore " & ControlChars.CrLf & _
+"magna aliquam erat volutpat. Ut wisi enim ad minim veniam, " & ControlChars.CrLf & _
+"quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut " & ControlChars.CrLf & _
+"aliquip ex ea commodo consequat. Duis autem vel eum iriure " & ControlChars.CrLf & _
+"dolor in hendrerit in vulputate velit esse molestie consequat, " & ControlChars.CrLf & _
+"vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan " & ControlChars.CrLf & _
+"et iusto odio dignissim qui blandit praesent luptatum zzril delenit " & ControlChars.CrLf & _
+"augue duis dolore te feugait nulla facilisi. Nam liber tempor cum " & ControlChars.CrLf & _
+"soluta nobis eleifend option congue nihil imperdiet doming id " & ControlChars.CrLf & _
+"quod mazim placerat facer possim assum."
+            End Get
+        End Property
+    End Class
+End Namespace
